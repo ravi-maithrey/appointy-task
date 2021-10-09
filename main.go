8742deab0111d0de99216319c3478c49aaa33f2d
ravi maithrey <maitrey.ind@gmail.com>
2021-10-09 20:17:48 +0530

    default

--- main.go
+++ main.go
@@ -2,50 +2,73 @@ package appointytask
 
 import (
 	"context" // to take care of tieouts and other signals
+	"encoding/json"
 	"fmt"
 	"log"      //to log errors
 	"net/http" //routing
 	"time"     //to have a timeout measure
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt" //password hashing
 )
 
 var client *mongo.Client
 
 type instagram struct {
-	User     user
-	password string
-	post     string
+	User []user
+	post string
 }
 type user struct {
-	userid    int    `json:"userid"`
-	username  string `json:"username"`
-	useremail string `json:useremail"`
+	userid    int    `json:"userid", db:"userid"`
+	username  string `json:"username", db:"username"`
+	useremail string `json:useremail", db:"useremail"`
+	password  string `json:"password", db:"password"`
 }
 
-func createUser() {
+func createUser(w http.ResponseWriter, r *http.Request) {
+	details := &user{}
+	_ = json.NewDecoder(r.Body).Decode(details)
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(details.password), 8)
+
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
+	usersDatabase := client.Database("user")
+	usersDetails := usersDatabase.Collection("user")
+	userResult, err := usersDetails.InsertOne(ctx, bson.D{
+		{Key: "userid", Value: details.userid},
+		{Key: "username", Value: details.username},
+		{Key: "useremail", Value: details.useremail},
+		{Key: "password", Value: hashedPassword},
+	},
+	)
+
+	json.NewEncoder(w).Encode(userResult)
+
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	details := &user.userid
+	_ = json.NewDecoder(r.Body).Decode(details)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
 }
 func main() {
-
-	mux = http.NewServeMux() //to handle routing
-
-	mux.HandleFunc("/users", createUser)
-	mux.HandleFunc(fmt.Sprintf("/users/%d", userid), getUser)
 
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://ravi:<password>@cluster0.bwqsb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer client.Disconnect(ctx)
+	var mux = http.NewServeMux() //to handle routing
+
+	mux.HandleFunc("/users", createUser)
+	mux.HandleFunc(fmt.Sprintf("/users/%d", userid), getUser)
+
 }
