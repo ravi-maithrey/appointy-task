ec2782d068976bd2a40765d2fb711dfff1d55ee1
ravi maithrey <maitrey.ind@gmail.com>
2021-10-09 21:56:00 +0530

    default

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package appointytask
 
 import (
-	"context" // to take care of tieouts and other signals
-	"encoding/json"
+	"context"       // to take care of tieouts and other signals
+	"encoding/json" //to decode and encode incoming and outgoing data
 	"fmt"
 	"log"      //to log errors
 	"net/http" //routing
@@ -16,15 +16,21 @@ import (
 
 var client *mongo.Client
 
-type instagram struct {
+type instagram struct { //structure of the total data
 	User []user
-	post string
+	Post []post
 }
-type user struct {
-	userid    int    `json:"userid", db:"userid"`
-	username  string `json:"username", db:"username"`
-	useremail string `json:useremail", db:"useremail"`
-	password  string `json:"password", db:"password"`
+type user struct { //structure of user data
+	userid    int    `json:"userid", bson:"userid"`
+	username  string `json:"username", bson:"username"`
+	useremail string `json:useremail", bson:"useremail"`
+	password  string `json:"password", bson:"password"`
+}
+type post struct { //structure of posts data
+	postid      int       `json:"postid", bson:"postid"`
+	postcaption string    `json:"postcaption", bson:"postcaption"`
+	imageurl    string    `json:"imageurl", bson:"imageurl"`
+	timestamp   time.Time `json:"timestamp", bson:"timestamp"`
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
@@ -56,26 +62,91 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	var details int
 	_ = json.NewDecoder(r.Body).Decode(details)
+
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err := client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
-	usersDatabase := client.Database("user")
-	usersDetails := usersDatabase.Collection("user")
+	usersDatabase := client.Database("users")
+	usersDetails := usersDatabase.Collection("userdetails")
 	userResult, err := usersDetails.Find(ctx, bson.M{"userid": details})
 	json.NewEncoder(w).Encode(userResult)
 }
-func main() {
 
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://ravi:<password>@cluster0.bwqsb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+func createPost(w http.ResponseWriter, r *http.Request) {
+	details := &post{}
+	_ = json.NewDecoder(r.Body).Decode(details)
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var mux = http.NewServeMux() //to handle routing
+	postsDatabase := client.Database("posts")
+	postDetails := postsDatabase.Collection("postdetails")
+	postResult, err := postDetails.InsertOne(ctx, bson.D{
+		{Key: "postid", Value: details.postid},
+		{Key: "postcaption", Value: details.postcaption},
+		{Key: "imageurl", Value: details.imageurl},
+		{Key: "timestamp", Value: details.timestamp},
+	},
+	)
+
+	json.NewEncoder(w).Encode(postResult)
+}
+
+func getPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	var details int
+	_ = json.NewDecoder(r.Body).Decode(details)
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	postsDatabase := client.Database("posts")
+	postDetails := postsDatabase.Collection("postdetails")
+	postResult, err := postDetails.InsertOne(ctx, bson.M{"postid": details})
+	json.NewEncoder(w).Encode(postResult)
+}
+
+func getAllPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	var details int
+	_ = json.NewDecoder(r.Body).Decode(details)
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	postsDatabase := client.Database("posts")
+	postDetails := postsDatabase.Collection("postdetails")
+	postResult, err := postDetails.InsertOne(ctx, bson.M{"postid": details})
+	json.NewEncoder(w).Encode(postResult)
+}
+
+func routing(r *http.Request) {
+	details := &instagram{}
+	_ = json.NewDecoder(r.Body).Decode(details)
+	mux := http.NewServeMux() //to handle routing
 
 	mux.HandleFunc("/users", createUser)
-	mux.HandleFunc(fmt.Sprintf("/users/%d", userid), getUser)
+	mux.HandleFunc(fmt.Sprintf("/users/%d", details.User[0].userid), getUser)
+	mux.HandleFunc("/posts", createPost)
+	mux.HandleFunc(fmt.Sprintf("/posts/%d", details.Post[0].postid), getPost)
+	mux.HandleFunc(fmt.Sprintf("/posts/users/%d", details.User[0].userid), getAllPosts)
+}
+func main() {
+	var url http.Request
+	_, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://ravi:ravi_password@cluster0.bwqsb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Scanln(&url)
+	routing(&url)
 
 }
