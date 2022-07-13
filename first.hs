dbee02503434c1a3e12783b67aba5d8688fa80fb
ravi maithrey <maitrey.ind@gmail.com>
2022-07-13 17:59:02 +0530

    from my pc

--- first.hs
+++ first.hs
@@ -4,9 +4,6 @@ import System.Environment
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let num1 = read args !! 0 :: Int
-  let num2 = read args !! 1 :: Int
-  let sum = num1 + num2
-  let value = show sum
-  putStrLn value
+  putStrLn "Enter your name"
+  name <- getLine
+  putStrLn ("Hello, " ++ name)
