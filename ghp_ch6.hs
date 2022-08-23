6122ec8f648c7fc09a1e095fdd3d49ed6ac6797c
ravi maithrey <maitrey.ind@gmail.com>
2022-08-23 18:51:42 -0400

    from my pc

--- ghp_ch6.hs
+++ ghp_ch6.hs
@@ -8,3 +8,11 @@ myRepeat n = cycle [n]
 and a list. The function should return the subsequence between the start and end.--}
 subseq start end list = take difference (drop start list)
     where difference = end - start
+
+{--
+Write a function inFirstHalf that returns True if an element is in the first half of a
+list, and otherwise returns False.
+--}
+-- since elem returns a bool anyway, we just return it and we drop the revrse of the list as we want the first half
+inFirstHalf element list = element `elem` (drop half (reverse list))
+    where half = length list `div` 2 -- div instead of / as / gives a frac int
