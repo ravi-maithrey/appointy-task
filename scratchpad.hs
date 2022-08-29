81e253f7a79fcbbb4d6a15c660d5e5b9f83e3735
ravi maithrey <maitrey.ind@gmail.com>
2022-08-29 11:42:11 -0400

    from my pc

--- scratchpad.hs
+++ scratchpad.hs
@@ -1,4 +1,10 @@
 runningSum :: [Int] -> [Int]
 runningSum (x1:x2:xs) = x1:(x1+x2):runningSum (x2:xs)
-runningSum [x] = [0+x]
-runningSum [] = [0]
+runningSum [x]        = [0+x]
+runningSum []         = [0]
+
+group3 [] = []
+group3 xs = g : group3 ys where (g,ys) = splitAt 3 xs
+
+-- the quick check property can be
+-- length (group3 xs) == ((length xs `div` 3) + (length xs `mod` 3))
