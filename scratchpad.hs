2f40293f7f4c3865f811c7173e18c0470f88e2ff
ravi maithrey <maitrey.ind@gmail.com>
2022-08-30 11:03:24 -0400

    from my pc

--- scratchpad.hs
+++ scratchpad.hs
@@ -1,3 +1,7 @@
+import           Test.Tasty
+import           Test.Tasty.HUnit      (Assertion, testCase, (@?=))
+import qualified Test.Tasty.QuickCheck as QC
+
 runningSum :: [Int] -> [Int]
 runningSum (x1:x2:xs) = x1:(x1+x2):runningSum (x2:xs)
 runningSum [x]        = [0+x]
@@ -8,3 +12,7 @@ group3 xs = g : group3 ys where (g,ys) = splitAt 3 xs
 
 -- the quick check property can be
 -- length (group3 xs) == ((length xs `div` 3) + (length xs `mod` 3))
+
+-- another property
+-- concat all lists after group3 == list before group 3
+-- xs == concat (group3 xs)
